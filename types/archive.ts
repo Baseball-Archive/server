9c3c9a50039d57f58b9660899530244e583968f2
bang-wol <milk011922@gmail.com>
2024-08-20 04:32:40 +0900

    Feat: ArchiveData 타입 정의

---
+++ types/archive.ts
@@ -0,0 +1,11 @@
+export interface ArchiveData {
+  schedule_id: number;
+  weather: string;
+  home_team_score: number;
+  away_team_score: number;
+  title: string;
+  content: string;
+  pic_url: string;
+  is_public: boolean;
+  user_uid: string;
+}

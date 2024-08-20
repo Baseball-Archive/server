f9e7868f9a10385e04e7701141ab98f816ba41aa
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2024-08-20 10:17:49 +0900

    Chore: 배포된 db와 연동 부분 수정

--- postgresql.ts
+++ postgresql.ts
@@ -9,6 +9,9 @@ const pool = new Pool({
   database: process.env.DATABASE,
   password: process.env.POSTGRESQL_PASSWORD,
   port: parseInt(process.env.POSTGRESQL_PORT || "5432", 10),
+
+  //이부분이 없으면 배포된 db와 연동이 실패한다. 보안상으로 다르게 설정할 필요가 있어보인다.
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;

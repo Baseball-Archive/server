78497e3cb12b7306d6e1a71483d8bd152dff8310
bang-wol <milk011922@gmail.com>
2024-08-21 09:26:49 +0900

    Refactor: route 변수명 통일하여 수정

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express, { Application } from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import privateArchiveRoutes from "./routes/privateArchive";
+import archiveRouter from "./routes/privateArchive";
 
 dotenv.config();
 
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/archive", privateArchiveRoutes);
+app.use("/archive", archiveRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);

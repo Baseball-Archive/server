b22d7f3da3057dd5b58701a9eed2224ccf28e3a8
bang-wol <milk011922@gmail.com>
2024-08-23 19:32:19 +0900

    Fix: import 경로 수정

--- src/controllers/scheduleController.ts
+++ src/controllers/scheduleController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { scrapeSchedule } from "../scraper/scheduleScraper";
+import { scrapeSchedule } from "../scrapers/scheduleScraper";
 import { saveSchedulesRepository, getSchedulesRepository } from "../Repositories/scheduleRepository";
 
 export const saveSchedules = async (req: Request, res: Response) => {
@@ -13,3 +13,16 @@ export const saveSchedules = async (req: Request, res: Response) => {
   }
 };
 
+export const getSchedules = async (req: Request, res: Response) => {
+  try {
+    const { date } = req.params;
+    const schedules = await getSchedulesRepository(date);
+    if (schedules.length > 0) {
+      res.status(StatusCodes.OK).json(schedules);
+    } else {
+      res.status(StatusCodes.NOT_FOUND).json({ message: "해당 날짜의 경기 일정을 찾을 수 없습니다." });
+    }
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "경기 일정을 불러오는데 실패했습니다." });
+  }
+};

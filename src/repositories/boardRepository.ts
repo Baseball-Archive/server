21c93ce124fee288f85e652fcd5b4a9cc3b91a0b
gixxla <gigixquisite@gmail.com>
2024-08-25 00:14:47 +0900

    Fix: 게시판 및 공개 아카이브 프론트 반환 데이터 수정

--- src/repositories/boardRepository.ts
+++ src/repositories/boardRepository.ts
@@ -5,9 +5,9 @@ export const addPostRepository = async (boardData: BoardData) => {
   const { scheduleId, title, content, picUrl, userUid } = boardData;
 
   const sql = `
-        INSERT INTO board (schedule_id, title, content, pic_url, user_uid)
-        VALUES ($1, $2, $3, $4, $5);
-    `;
+    INSERT INTO board (schedule_id, title, content, pic_url, user_uid)
+    VALUES ($1, $2, $3, $4, $5);
+  `;
   const values = [scheduleId, title, content, picUrl, userUid];
 
   await pool.query(sql, values);
@@ -17,9 +17,9 @@ export const updatePostRepository = async (boardId: number, boardData: BoardData
   const { scheduleId, title, content, picUrl } = boardData;
 
   const sql = `
-        UPDATE board SET schedule_id = $1, title = $2, content = $3, pic_url = $4
-        WHERE id = $5;
-    `;
+    UPDATE board SET schedule_id = $1, title = $2, content = $3, pic_url = $4
+    WHERE id = $5;
+  `;
   const values = [scheduleId, title, content, picUrl, boardId];
 
   await pool.query(sql, values);
@@ -27,18 +27,24 @@ export const updatePostRepository = async (boardId: number, boardData: BoardData
 
 export const deletePostRepository = async (boardId: number) => {
   const sql = `
-        DELETE FROM board WHERE id = $1 
-    `;
+    DELETE FROM board WHERE id = $1 
+  `;
 
   await pool.query(sql, [boardId]);
 };
 
 export const findAllPostRepository = async () => {
   const sql = `
-        SELECT home_team_id, away_team_id, title, created_at, user_uid
-        FROM board LEFT JOIN baseball_schedule
-        ON board.schedule_id = baseball_schedule.id;
-    `;
+    SELECT board.id, home_team_id, away_team_id, title, board.created_at, nickname, baseball_team.name,
+    (SELECT COUNT(*) FROM like_board WHERE board_id = board.id) AS likes,
+    (SELECT COUNT(*) FROM comment_board WHERE board_id = board.id) AS comments
+    FROM board LEFT JOIN baseball_schedule
+    ON board.schedule_id = baseball_schedule.id
+    LEFT JOIN users
+    ON board.user_uid = users.uid
+    LEFT JOIN baseball_team
+    ON users.my_team_id = baseball_team.id;
+  `;
 
   const result = await pool.query(sql);
   return result.rows;
@@ -46,13 +52,17 @@ export const findAllPostRepository = async () => {
 
 export const findPostRepository = async (boardId: number) => {
   const sql = `
-        SELECT match_date, home_team_id, away_team_id, title, content, pic_url, created_at, user_uid,
-        (SELECT COUNT(*) FROM like_board WHERE board_id = $1) AS likes,
-        (SELECT COUNT(*) FROM comment_board WHERE board_id = $1) AS comments
-        FROM board LEFT JOIN baseball_schedule
-        ON board.schedule_id = baseball_schedule.id
-        WHERE board.id = $1 
-    `;
+    SELECT board.id, match_date, home_team_id, away_team_id, title, content, board.pic_url, board.created_at, nickname, baseball_team.name,
+    (SELECT COUNT(*) FROM like_board WHERE board_id = $1) AS likes,
+    (SELECT COUNT(*) FROM comment_board WHERE board_id = $1) AS comments
+    FROM board LEFT JOIN baseball_schedule
+    ON board.schedule_id = baseball_schedule.id
+    LEFT JOIN users
+    ON board.user_uid = users.uid
+    LEFT JOIN baseball_team
+    ON users.my_team_id = baseball_team.id
+    WHERE board.id = $1 
+  `;
 
   const result = await pool.query(sql, [boardId]);
   return result.rows[0];
--- src/repositories/publicArchiveRepository.ts
+++ src/repositories/publicArchiveRepository.ts
@@ -2,11 +2,17 @@ import pool from "../../config/postgresql";
 
 export const findAllArchiveRepository = async () => {
   const sql = `
-          SELECT home_team_id, away_team_id, title, created_at, user_uid
-          FROM archive LEFT JOIN baseball_schedule
-          ON archive.schedule_id = baseball_schedule.id
-          WHERE is_public = true;
-      `;
+    SELECT archive.id, home_team_id, away_team_id, title, archive.created_at, nickname, baseball_team.name,
+    (SELECT COUNT(*) FROM like_archive WHERE archive_id = archive.id) AS likes,
+    (SELECT COUNT(*) FROM comment_archive WHERE archive_id = archive.id) AS comments
+    FROM archive LEFT JOIN baseball_schedule
+    ON archive.schedule_id = baseball_schedule.id
+    LEFT JOIN users
+    ON archive.user_uid = users.uid
+    LEFT JOIN baseball_team
+    ON users.my_team_id = baseball_team.id
+    WHERE is_public = true;
+  `;
 
   const result = await pool.query(sql);
   return result.rows;
@@ -14,13 +20,17 @@ export const findAllArchiveRepository = async () => {
 
 export const findArchiveRepository = async (archiveId: number) => {
   const sql = `
-          SELECT match_date, home_team_id, away_team_id, title, content, pic_url, created_at, user_uid,
-          (SELECT COUNT(*) FROM like_archive WHERE archive_id = $1) AS likes,
-          (SELECT COUNT(*) FROM comment_archive WHERE archive_id = $1) AS comments
-          FROM archive LEFT JOIN baseball_schedule
-          ON archive.schedule_id = baseball_schedule.id
-          WHERE archive.id = $1 
-      `;
+    SELECT archive.id, match_date, home_team_id, away_team_id, title, content, archive.pic_url, archive.created_at, nickname, baseball_team.name,
+    (SELECT COUNT(*) FROM like_archive WHERE archive_id = $1) AS likes,
+    (SELECT COUNT(*) FROM comment_archive WHERE archive_id = $1) AS comments
+    FROM archive LEFT JOIN baseball_schedule
+    ON archive.schedule_id = baseball_schedule.id
+    LEFT JOIN users
+    ON archive.user_uid = users.uid
+    LEFT JOIN baseball_team
+    ON users.my_team_id = baseball_team.id
+    WHERE archive.id = $1 
+  `;
 
   const result = await pool.query(sql, [archiveId]);
   return result.rows[0];

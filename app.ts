874d422f0585e601d00cd88fbe65217050b274ae
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2024-08-24 22:11:09 +0900

    Fix: app.ts cors 적용

--- app.ts
+++ app.ts
@@ -8,10 +8,19 @@ import usersRouter from "./src/routes/users";
 import uploadRouter from "./src/routes/awsUpload";
 import scheduleRouter from "./src/routes/schedule";
 import rankingRouter from "./src/routes/ranking";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  }),
+);
 
 app.use(cookieParser());
 app.use(express.json());
